2b0f329ae1428e3a507a9cd0f98d105cdf73a918
dayee0318 <dy0318m@gmail.com>
2023-04-10 18:21:05 -0600

    picoCTF Mod26 - solved

---
+++ ROT13.py
@@ -0,0 +1,14 @@
+message = input("Enter the message to decode: ")
+result = ""
+
+for char in message:
+    if char.isalpha():
+        if char.isupper():
+            result += chr((ord(char) - 65 + 13) % 26 + 65)
+        else:
+            result += chr((ord(char) - 97 + 13) % 26 + 97)
+    else:
+        result += char
+
+print(result)
+
